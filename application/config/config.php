a3f6e9aa2b644c49c1272e364c9e7c35ea44545a
Ansuman Mishra <sunmoonfortune@gmail.com>
2013-02-17 16:09:57 +0530

    Changed the Folder Path

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/codeigniter-initializr-ionauth/';
+$config['base_url']	= 'http://127.0.0.1/CodeIgniter-Initilizr-IonAuth/';
 
 /*
 |--------------------------------------------------------------------------
